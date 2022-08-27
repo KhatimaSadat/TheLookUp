719c1941d9156114a8fa3f6eefca9f2b87780da1
KaynatRisha <112246779+KaynatRisha@users.noreply.github.com>
2022-08-27 02:41:29 -0700

    done

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,27 @@
+import {Spin as Humburger} from 'hamburger-react';
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import MenuIcon from "@mui/icons-material/Menu";
-
 const NavBar = () => {
-   
-    return (  
-    <div className="navbar">
-         <div className="left-side">
-             <div className="menuicon" ><MenuIcon fontSize="small"/> </div>
-              <div className="text">TheLookUP</div>
-         </div>
-         <div className="right-side">
-            <Link to="/">HOME</Link> 
-            <Link to="/AboutUs">ABOUT US</Link>
-            <Link to="/featurs">FEATURES</Link>
-            <Link to="/contact">CONTACT</Link>
-            <Link to="/login">LOG IN</Link>
-            
-         </div>
-
-     </div>
+  return (  
+         <nav className='navbar'>
+          <nav className="left-side">
+            <div className="humburgerIcon"> <Humburger  rounded color='white' size={30} /></div>
+             <h2 className="lookup">Thelookup</h2>
+          </nav> 
+          <nav className='right-side'>
+             <Link to="/" ><span className="span-home-nav" > HOME</span> </Link>
+             <Link to="/AboutUs">ABOUT US</Link>
+             <Link to="/featurs">FEATURES</Link>
+             <Link to="/contact">CONTACT</Link>
+             <Link to="/login">LOG IN</Link>
+          </nav>
+          <div className="sidebare"></div>
+          </nav>
     );
 }
- 
 export default NavBar;
+
+
+
+
+
+ 
